bcf1c3839c4ed0c924bd00151ceaf3287934349c
khajaumar <khajau460@gmail.com>
2021-08-11 23:55:22 -0700

    split

---
+++ Api/subExists.js
@@ -0,0 +1,9 @@
+
+
+const subExists = (str) => {
+   return str.split(" ").filter((item)=>item=="Lorem");
+};
+const strData="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
+console.log(subExists(strData));
+
